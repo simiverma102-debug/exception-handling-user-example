3451dbcdef2c7f7bc84aef054f762ff737c7a166
Simran Verma <simi.verma.102@gmail.com>
2026-09-15 10:21:54 -0700

    login_01 passed

--- login_01.py
+++ login_01.py
@@ -4,11 +4,13 @@ def login_01():
         "name": "Crow T. Robot",
         "id": 11067
     }
-    
-    if user_data["is_logged_in"]:
-        print(f"{user_data['name']}, "
-            "you've successfully logged in!")
-    else:
-        print("You must log in to continue.")
-
+    try:                                            #try clause
+        if user_data["is_logged_in"]:
+            print(f"{user_data['name']}, "
+                "you've successfully logged in!")
+        else:
+            print("You must log in to continue.")
+    except KeyError as error:                        #except clause
+        print("you have entered a key that " 
+        f"doesn't exist in 'user_data: {error}")
 login_01()
--- login_02.py
+++ login_02.py
@@ -4,16 +4,14 @@ def login_02():
         "name": "Crow T. Robot",
         "id": 11067
     }
-    
-    try:
+    if user_data.get("is_logged_in"):
         if user_data["is_logged_in"]:
             print(f"{user_data['name']}, "
                 "you've successfully logged in!")
         else:
             print("You must log in to continue.")
             
-    except KeyError as err :
-        print("You've entered a key that "
-            f"doesn't exist in 'user_data': {err}")
+    else:
+        print("error")
 
 login_02()
